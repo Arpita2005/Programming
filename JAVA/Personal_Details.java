21d1f51418388bed740ae428574c066eeeda4b87
Arpita2005 <arpita.maitra2005@gmail.com>
2022-12-01 17:50:22 +0530

    new file:   JAVA/Personal_Details.class 	new file:   JAVA/Personal_Details.java 	new file:   JAVA/calc.class 	new file:   JAVA/calc.java

---
+++ JAVA/Personal_Details.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Personal_Details
+{
+   public static void main(String[] args)
+   {
+		System.out.println("Name: Arpita Maitra");
+		System.out.println("College Name:NSHM");
+		System.out.println("Education: BCA");
+		System.out.println("Father's Name: Prabir Maitra");
+		System.out.print("Father's Occupation: Job");
+		System.out.println("Hobby: Drawing,Cooking");
+	}
+}
+		
---
+++ JAVA/calc.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class calc
+{
+	public static void main(String[] args)
+	{
+	int i= 21,j=4,k;
+	k=i+j;
+	System.out.println("Sum: "+k);
+	k=i-j;
+	System.out.println("Subtraction: "+k);
+	k=i*j;
+	System.out.println("Multiplication: "+k);
+	k=i/j;
+	System.out.println("Division: "+k);
+	k=i%j;
+	System.out.println("Reminder: "+k);
+	}
+	}
+	
